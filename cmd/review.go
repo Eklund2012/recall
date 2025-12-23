4019fbd4de0fec98e6552b70ea0917af7f00e6e0
Eklund2012 <david.eklund9@gmail.com>
2025-12-23 16:20:38 +0100

    Remove commented-out flag definitions from review command initialization

--- cmd/review.go
+++ cmd/review.go
@@ -31,14 +31,4 @@ func runReview(cmd *cobra.Command, args []string) {
 
 func init() {
 	rootCmd.AddCommand(reviewCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// reviewCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// reviewCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
